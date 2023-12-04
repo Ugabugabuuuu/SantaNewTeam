e5dd0afc98ed36154dc23e7b0c937fa82d0aa442
Gytis0 <gytiskupcinskas0@gmail.com>
2023-12-04 19:53:57 +0200

    Added enum

---
+++ User_Input.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+    enum User_Input
+    {
+        Register = 2,
+        Login = 1,
+
+
+        Exit = 0,
+        Add_New_Child = 1,
+        Add_New_Gift = 2,
+        Add_Spec_Gift_To_Kid = 3,
+        Add_Spec_Gift_To_Rand_Kid = 4,
+        Rand_Gift_To_Rand_Kid = 5,
+        Lazy_Mode = 6,
+        Display_All_Kids = 7,
+        Display_Gift_List = 8,
+        Display_Unassignes = 9,
+        Change_Status = 10,
+        Display_User_Info = 11,
+        Update_User_Info = 12
+    }
+
